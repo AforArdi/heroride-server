5daf2ed9770a150b1ba4a2b99af7b5b4681e2ca9
AforArdi <binsbro36@gmail.com>
2026-05-18 15:32:29 +0600

    mongodb added

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const run =async ()=>{
+    try{
+        await client.connect();
+
+        // opther database operation
+
+        await client.db("heroridedb").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Server is running fine!')
